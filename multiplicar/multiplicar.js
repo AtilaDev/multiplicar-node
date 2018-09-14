467947b8f99130fb909a52a9e530ddae7be7242d
AtilaDev <atiladevelop@gmail.com>
2018-09-14 16:27:33 -0300

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const colors = require('colors');
+
+listarTabla = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+    if (!Number(base)) {
+      reject(`El valor introducido ${base} no es un número`);
+      return;
+    }
+
+    let data = '';
+
+    console.log('=================='.green);
+    console.log(`Tabla del ${base}`.green);
+    console.log('=================='.green);
+
+    for (let i = 1; i <= limite; i++) {
+      data += `${base} * ${i} = ${base * i}\n`;
+    }
+
+    resolve(data);
+  });
+};
+
+crearArchivo = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+    if (!Number(base)) {
+      reject(`El valor introducido ${base} no es un número`);
+      return;
+    }
+
+    let data = '';
+
+    for (let i = 1; i <= limite; i++) {
+      data += `${base} * ${i} = ${base * i}\n`;
+    }
+
+    fs.writeFile(`./tablas/tabla-${base}.txt`, data, err => {
+      if (err) reject(err);
+      else resolve(`tabla-${base}.txt`);
+    });
+  });
+};
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+};
